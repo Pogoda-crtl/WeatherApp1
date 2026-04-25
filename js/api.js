99616fcd478047695af7d452dab5b4dad40f148e
hubertsel12234-hash <hubertsel12234@gmail.com>
2026-04-25 20:03:20 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,35 @@
+const API_KEY = '9690e96730acb4865ed43fe11cf36eb5';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
+const WeatherAPI = {
+    
+    getWeatherByCity: async (city, unit = 'metric') => {
+        try {
+            const response = await fetch(`${BASE_URL}/weather?q=${city}&units=${unit}&appid=${API_KEY}&lang=pl`);
+            if (!response.ok) throw new Error('Nie znaleziono miasta');
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getForecastByCity: async (city, unit = 'metric') => {
+        try {
+            const response = await fetch(`${BASE_URL}/forecast?q=${city}&units=${unit}&appid=${API_KEY}&lang=pl`);
+            if (!response.ok) throw new Error('Błąd pobierania prognozy');
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getWeatherByCoords: async (lat, lon, unit = 'metric') => {
+        try {
+            const response = await fetch(`${BASE_URL}/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${API_KEY}&lang=pl`);
+            if (!response.ok) throw new Error('Błąd lokalizacji');
+            return await response.json();
+        } catch (error) {
+            throw error;
+        }
+    }
+};
