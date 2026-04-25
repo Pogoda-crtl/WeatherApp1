ba2fd755a9e69ccc3facd613d596d802c810d1d0
hubertsel12234-hash <hubertsel12234@gmail.com>
2026-04-25 20:02:44 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,29 @@
+const STORAGE_KEY = 'weatherHistory';
 
+const StorageAPI = {
+    saveSearch: (city) => {
+        try {
+            let history = StorageAPI.getHistory();
+            
+            history = history.filter(c => c.toLowerCase() !== city.toLowerCase());
+            history.unshift(city); 
+            
+            if (history.length > 5) history.pop();
+            
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(history));
+            return history;
+        } catch (error) {
+            console.error('Błąd zapisu do localStorage:', error);
+        }
+    },
+
+    getHistory: () => {
+        try {
+            const data = localStorage.getItem(STORAGE_KEY);
+            return data ? JSON.parse(data) : [];
+        } catch (error) {
+            console.error('Błąd odczytu z localStorage:', error);
+            return [];
+        }
+    }
+};
